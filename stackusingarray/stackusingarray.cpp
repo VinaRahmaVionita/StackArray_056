342263db8a61c8a8ed048a2b60872477737e5532
VinaRahmaVionita <vinarahmavionita824@gmail.com>
2023-06-16 15:02:17 +0700

    membuat class atck array dengan accses private dan ukuran array adalah 5

--- stackusingarray/stackusingarray.cpp
+++ stackusingarray/stackusingarray.cpp
@@ -8,37 +8,4 @@ private:
 	int stack_array[5];
 	int top;
 
-public:
-	//construktor
-	StackArray() {
-		top = -1;
-	}
-	int push(int element) {
-		if (top == 4) {
-			cout << "number of data exceed the limit" << endl;
-			return 0;
-		}
-
-		top++;   //step 2
-		stack_array[top] = element; //step 3
-		cout << endl;
-		cout << element << "ditambahkan(pushed)" << endl;
-
-		return element;
-	}
-	void pop() {
-		if (empty()) {  //step 1
-			cout << "\nStack is empty. Cannot op." << endl; //1.a
-			return; //1.b
-		}
-		
-		cout << "\nthe pop element is : " << stack_array[top] << endl;  //step 2
-		top--;  //step 3 decrement
-	}
-
-	//method for check if data is empty
-	bool empty() {
-
-	}
-};
 
